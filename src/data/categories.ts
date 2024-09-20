fdad5a9c8bbde1145a5c2cef144edc9d549a6c21
mindswonders <brain.art.magazine@gmail.com>
2024-09-20 21:21:55 +0200

    Categorie

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Neuroscience & Artistry',
+	'Thinking Tech',
+	'Minding Philosophy',
+	'Clinical Peculiarity',
+	'Women & Wellness'
 ] as const
